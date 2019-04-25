ccb44ec270cd03005711abf9344eb6b73e249d53
farka <farkasnorbert12@gmail.com>
2019-04-25 14:11:47 +0300

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,9 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Main {
 
-    static int n;
     public static void main(String[] args) {
 
     }
-    public static int[][] in(){
-        try {
-            Scanner scanner = new Scanner(new File("be.txt"));
-            n=scanner.nextInt();
-            int[][] V = new int[n][n];
-            int m = scanner.nextInt();
-            int x,y,z;
-            for(int i=0; i<m;i++){
-                x = scanner.nextInt();
-                y = scanner.nextInt();
-                z = scanner.nextInt();
-                V[x][y]=z;
-                V[y][x]=z;
-            }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 }
